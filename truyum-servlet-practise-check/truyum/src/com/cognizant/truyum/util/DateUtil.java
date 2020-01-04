5863dadfdfc4042a96082e3703dd2378778c4054
Hemant Prasad <hemantprasad99.HP@gmail.com>
2020-01-04 12:49:10 +0530

    Add files via upload
    truYum servlet practise check added

---
+++ truyum-servlet-practise-check/truyum/src/com/cognizant/truyum/util/DateUtil.java
@@ -0,0 +1,24 @@
+package com.cognizant.truyum.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+	public Date convertToDate(String input) {
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			Date dateOfLaunch = format.parse(input);
+			return dateOfLaunch;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String convertToString(Date input) {
+		SimpleDateFormat dformat = new SimpleDateFormat("dd/MM/yyyy");
+		String date = dformat.format(input);
+		return date;
+	}
+}
